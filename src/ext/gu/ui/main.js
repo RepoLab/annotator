96110cee2241b690500eedf53e121018c861859a
CNDLS <cndls_developers@georgetown.edu>
2015-09-22 14:10:53 -0400

    add gu-scoped code.

---
+++ src/ext/gu/ui/main.js
@@ -0,0 +1,13 @@
+/*package annotator.ext.gu.ui */
+"use strict";
+
+var util = require('../../../util');
+
+var adder = require('./adder');
+var editor = require('./editor');
+var highlighter = require('../../../ui/highlighter');
+var textselector = require('../../../ui/textselector');
+var viewer = require('./viewer');
+
+
+exports.main = main;
---
+++ test/spec/ext/gu/ui_spec.js
@@ -0,0 +1,56 @@
+var assert = require('assertive-chai').assert;
+
+var adder = require('../../../../src/ext/gu/ui/adder');
+var editor = require('../../../../src/ext/gu/ui/editor');
+
+var highlighter = require('../../../../src/ui/highlighter');
+var textselector = require('.../../../../src/ui/textselector');
+
+var viewer = require('../../../../src/ext/gu/ui/viewer');
+
+var gu_ui = require('../../../../src/ext/gu/ui/main.js');
+
+
+describe("annotator.ext.gu.ui module", function () {
+  var sandbox;
+  var mockAuthz;
+  var mockIdent;
+  var mockApp;
+
+  beforeEach(function () {
+      sandbox = sinon.sandbox.create();
+      mockAuthz = {
+          permits: sandbox.stub().returns(true),
+          authorizedUserId: function (u) { return u; }
+      };
+      mockIdent = {who: sandbox.stub().returns('alice')};
+      mockApp = {
+          annotations: {create: sandbox.stub()},
+          registry: {
+              getUtility: sandbox.stub()
+          }
+      };
+      mockApp.registry.getUtility.withArgs('authorizationPolicy').returns(mockAuthz);
+      mockApp.registry.getUtility.withArgs('identityPolicy').returns(mockIdent);
+  });
+
+  afterEach(function () {
+      sandbox.restore();
+      h.clearFixtures();
+  });
+
+  it('should attach the TextSelector to the document body by default', function () {
+      sandbox.stub(textselector, 'TextSelector');
+
+      var plug = gu_ui();
+      plug.start(mockApp);
+
+      sinon.assert.calledWith(textselector.TextSelector, document.body);
+  });
+  
+  it("loads modules from both annotator.ui and annotator.ext.gu.ui", function () {
+    assert.isFalse(gu_ui===null);
+  });
+});
+
+
